a8b0fc3d8a56ae46c9c9c2874efb0612091dc508
IbragimovOlimjon <ibragimovolimjon339@gmail.com>
2022-08-16 22:30:09 +0300

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// module.exports = {
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'hero-pattern': "url('/img/hero-pattern.svg')",
+//         'footer-texture': "url('/img/footer-texture.png')",
+//       }
+//     }
+//   }
+// },
+// module.exports = {
+//   theme: {
+//     backgroundSize: {
+//       'auto': 'auto',
+//       'cover': 'cover',
+//       'contain': 'contain',
+//       '50%': '50%',
+//       '16': '4rem',
+//     }
+//   }
+// }
